67d656a7907ed3d577f458075fd63b6011cad9c6
careerswitch <abolat01@gmail.com>
2026-02-18 21:18:32 +0200

    FIX: events.py to sync with GCS

--- events.py
+++ events.py
@@ -6,9 +6,12 @@ from datetime import datetime, timedelta
 import logging
 from fake_useragent import UserAgent
 import pytz
+import os
+import icalendar
+from google.cloud import storage
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='scraper_log.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 # Function to make HTTP request with proper error handling
@@ -30,8 +33,8 @@ def is_dst(date):
     return timezone.localize(date, is_dst=None).dst() != timedelta(0)
 
 
-# Function to scrape financial data
-def scrape_financial_data():
+# Function to scrape events calendar
+def scrape_events_calendar():
     try:
         url = os.environ.get("EVENT_URL")
         response = make_request(url)
@@ -62,16 +65,67 @@ def scrape_financial_data():
                     time_str_plus_3 = time.strftime("%I:%M %p")
                     events.append(
                         {"date": current_date.strftime("%A %B %d %Y"), "time": time_str_plus_3, "name": event})
-
-        # Print the scraped events to the console
-        for event in events:
-            print(f"Date: {event['date']}, Time: {event['time']}, Event: {event['name']}")
-
         return events
     except Exception as e:
-        logging.error(f"An error occurred while scraping financial data: {str(e)}")
+        logging.error(f"An error occurred while scraping the events website: {str(e)}")
+        return None
+
+
+# Function to save calendar to Google Cloud Storage
+def save_calendar_to_gcs(calendar):
+    try:
+        # Save the calendar file locally
+        filename = "Market.ics"
+        with open(filename, 'wb') as f:
+            f.write(calendar.to_ical())
+
+        # Upload the calendar file to GCS
+        client = storage.Client() # Changed to use default credentials, which should be set by github action
+        bucket_name = "market-calendar-bucket"
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(filename)
+        blob.upload_from_filename(filename)
+
+        logging.info(f"Calendar uploaded to Google Cloud Storage: gs://{bucket_name}/{filename}")
+
+        # Optional: Remove the local file after uploading to GCS
+        os.remove(filename)
+
+    except Exception as e:
+        logging.error(f"An error occurred while saving the calendar to Google Cloud Storage: {str(e)}")
+
+
+# Function to update the calendar
+def update_calendar():
+    # Scrape events calendar
+    economic_events = scrape_events_calendar()
+    if economic_events:
+        economics_calendar = icalendar.Calendar()
+        for event in economic_events:
+            dt = datetime.strptime(event["date"] + " " + event["time"], "%A %B %d %Y %I:%M %p")
+            ical_event = icalendar.Event()
+            ical_event.add("summary", event["name"])
+            ical_event.add("dtstart", dt)
+            ical_event.add("dtend", dt + timedelta(hours=1))
+            ical_event.add("dtstamp", datetime.now())
+            economics_calendar.add_component(ical_event)
+
+        # Save and upload the calendar to GCS
+        save_calendar_to_gcs(economics_calendar)
+
+    else:
+        logging.warning("No events found on the events calendar.")
 
 
 if __name__ == "__main__":
-    logging.info("Starting financial data scraping process")
-    scrape_financial_data()
+    logging.info("Starting calendar update process")
+
+    # Add logging to print the log file path
+    log_file_path = os.path.abspath('scraper_log.log')
+    logging.info(f"Log file path: {log_file_path}")
+
+    update_calendar()
+
+    # Add logging to print the log file path after execution
+    log_file_path_after = os.path.abspath('scraper_log.log')
+    logging.info(f"Log file path after execution: {log_file_path_after}")
